49f46b2d4b3c5bf5e73c09a8aa684d07902f5c0b
HYDRAISHI <82408105+HYDRAISHI@users.noreply.github.com>
2021-08-20 21:36:54 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,38 @@
+class Food {
+    constructor(){
+        this.image = loadImage("images/Milk.png");
+        this.foodStock = foodStock;        
+        this.lastFed = lastFed;   
+        
+    }
+
+    getFoodStock(){
+        return this.foodStock;
+    }   
+
+    updateFoodStock(foodStock){
+        this.foodStock = foodStock;
+    }
+
+    deductFood(){
+        if(foodS > 0){
+            foodS = foodS -1;
+        } 
+    }   
+
+    display(){
+        var x=50,y=200; 
+        image(this.image,600,200,50,50); 
+        if(this.foodStock!=0){ 
+            for(var i=0;i<this.foodStock;i++){
+                 if(i%10===0){ 
+                    x=80; 
+                    y=y+50; 
+                } 
+                image(this.image,x,y,50,50); 
+                x=x+30;
+                
+            }
+        }
+    }
+}
